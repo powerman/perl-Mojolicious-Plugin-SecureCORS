66620a56affbe58fdb860553688c46a69ec17dfc
Alex Efros <powerman@cpan.org>
2014-12-24 09:59:32 +0000

    version 1.0.3

--- lib/Mojolicious/Plugin/SecureCORS.pm
+++ lib/Mojolicious/Plugin/SecureCORS.pm
@@ -6,7 +6,7 @@ use utf8;
 use feature ':5.10';
 use Carp;
 
-use version; our $VERSION = qv('1.0.2');    # REMINDER: update Changes
+use version; our $VERSION = qv('1.0.3');    # REMINDER: update Changes
 
 # REMINDER: update dependencies in Build.PL
 use Mojo::Base 'Mojolicious::Plugin';
